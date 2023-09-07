83a8adb2742e4e522e09e738d6e454d39a23a556
Pikachu ツ <91775058+pikaws@users.noreply.github.com>
2023-09-07 15:32:29 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,11 @@ from heroku3 import from_key
 from pyrogram import Client
 from pyromod import listen
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-APP_NAME = os.environ.get("APP_NAME", None)
-API_KEY = os.environ.get("API_KEY", None)
+API_ID = int(os.environ.get("API_ID", 18049084))
+API_HASH = os.environ.get("API_HASH", "7e74b1e22026fcc291d32b3d431aa21e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6307400433:AAGn1blB30MlLKpk1cI1xL6tUOPZ-cYEAp4")
+APP_NAME = os.environ.get("APP_NAME", "StringSessionBot")
+API_KEY = os.environ.get("API_KEY", "rnd_ZP8b5Peo73AU0azyjHjdQw68hPu2")
 HU_APP = from_key(API_KEY).apps()[APP_NAME]
 
 bot = Client(":memory:",
